57d3fc7910b98997068853f99946931d483901d8
Caleb Rice <caleb.rice2001@gmail.com>
2018-12-04 23:40:15 -0500

    Added markers functionaliy via "Show sites" and "hide sites" buttons Reorganized files

--- js/app.js
+++ js/app.js
@@ -9,7 +9,13 @@ function initMap() {
     center: {lat: 34.101331, lng: -84.519381},
     zoom: 17
   });
-
+  var locations = [
+    {title: 'Bob the Turkey', location: {lat: 34.099842, lng: -84.519943}},
+    {title: 'Pie Bar', location: {lat: 34.099400, lng: -84.520342}},
+    {title: 'Copper Coin', location: {lat: 34.099661, lng: -84.518967}},
+    {title: 'Elm Street Arts Theater', location: {lat: 34.101792, lng: -84.519891}},
+    {title: 'Woodstock Events Green', location: {lat: 34.100189, lng: -84.521686}},
+  ];
 
   var largeInfowindo = new google.maps.InfoWindow();
 
